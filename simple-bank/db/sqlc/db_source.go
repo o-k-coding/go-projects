fdc99ababa1b1abc300982f169b2e90e7838ec81
Michael O'Keefe <okeefem355@gmail.com>
2022-06-10 23:48:11 -0400

    check in new wf file XIV

--- simple-bank/db/sqlc/db_source.go
+++ simple-bank/db/sqlc/db_source.go
@@ -9,13 +9,24 @@ import (
 
 func buildPostgresDBSource() (string, error) {
 	// In a real project I would have a package devoted to config values, and get these from that. But for now this is good enough
-	err := godotenv.Load("../../.env")
+	if _, err := os.Stat("../../.env"); err == nil {
+		fmt.Println("Env file exists, attempting to load env file")
+		err = godotenv.Load("../../.env")
+
+		if err != nil {
+			fmt.Println("Error loading env file")
+			return "", err
+		}
+	} else {
+		fmt.Println("Env file does not exist, lets hope the env vars exist")
+	}
+
 	user := os.Getenv("POSTGRES_USER")
 	password := os.Getenv("POSTGRES_PASSWORD")
 	host := os.Getenv("POSTGRES_HOST")
 	port := os.Getenv("POSTGRES_PORT")
 	db := os.Getenv("POSTGRES_DB")
-	// sslMode := os.Getenv("POSTGRES_SSL_MODE")
+	sslMode := os.Getenv("POSTGRES_SSL_MODE")
 
-	return fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable", user, password, host, port, db), err
+	return fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=%s", user, password, host, port, db, sslMode), err
 }
