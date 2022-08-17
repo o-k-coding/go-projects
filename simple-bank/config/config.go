9fb5a9b538a68a71f00d3e7b1a40db07975a34d5
Michael O'Keefe <okeefem355@gmail.com>
2022-08-16 21:37:50 -0400

    Set env defaults

--- simple-bank/config/config.go
+++ simple-bank/config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"log"
+	"os"
 	"time"
 
 	"github.com/spf13/viper"
@@ -17,30 +19,52 @@ type Config struct {
 	AccessTokenDuration time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
 }
 
-func LoadConfig(path string) (*Config, error) {
-	// viper.AddConfigPath(path)
-	// This is how you would specify the name of the config file, ex app.env
-	// viper.SetConfigName(".env")
-	// viper.SetConfigType("env")
+func setEnvDefaults() {
+	viper.SetDefault("POSTGRES_HOST", "localhost")
+	viper.SetDefault("POSTGRES_USER", "postgres")
+	viper.SetDefault("POSTGRES_PASSWORD", "")
+	viper.SetDefault("POSTGRES_DB", "simple-bank")
+	viper.SetDefault("POSTGRES_PORT", "5432")
+	viper.SetDefault("POSTGRES_SSL_MODE", "disable")
+	viper.SetDefault("TOKEN_SYMMETRIC_KEY", "")
+	viper.SetDefault("ACCESS_TOKEN_DURATION", "10m")
+}
 
-	// err := viper.ReadInConfig()
+func LoadConfig(path string) (*Config, error) {
+	viper.AddConfigPath(path)
+	// This is how you would specify the name of the config file, ex app.env
+	viper.SetConfigName(".env")
+	viper.SetConfigType("env")
+
 	// If the env variables exist in the env, they will overwrite the file values
 	viper.AutomaticEnv()
+	setEnvDefaults()
 
-	// if err != nil {
-	// 	if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-	// 		// Attempt to read from env variables
-	// 		log.Println(".env file not found, hopefully you set the env variables!")
-	// 	} else {
-	// 		return nil, err
-	// 	}
-	// }
+	err := viper.BindEnv("POSTGRES_DB")
+
+	if err != nil {
+		log.Println("error binding env var")
+	}
+	err = viper.ReadInConfig()
+
+	if err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			// Attempt to read from env variables
+			log.Println(".env file not found, hopefully you set the env variables!")
+		} else {
+			return nil, err
+		}
+	}
 
 	var config Config
 
-	err := viper.Unmarshal(&config)
+	err = viper.Unmarshal(&config)
 	if err != nil {
 		return nil, err
 	}
+	log.Println("config db")
+	log.Println(config.PostgresDB)
+	log.Println("env db")
+	log.Println(os.Getenv("POSTGRES_DB"))
 	return &config, nil
 }
